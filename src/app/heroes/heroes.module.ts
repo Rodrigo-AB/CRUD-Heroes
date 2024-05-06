e1ad298ecdd88e8de6a528d7d814702ba32b7f01
Rodrigo-AB <p_rodrigo_ab@hotmail.com>
2024-05-06 14:06:33 -0600

    terminada la seccion 13, y las paginas listas para ser usadas y decoradas con el angular material

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule
+  ]
+})
+export class HeroesModule { }
