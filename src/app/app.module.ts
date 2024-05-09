6eb20898622fe14591e8ad89304e8b5f3fb42327
Rodrigo-AB <p_rodrigo_ab@hotmail.com>
2024-05-09 11:25:01 -0600

    hasta este punto las tarjetas de los personajes estan lista, ahora falta optmizar la parte de busqueda y mas herramientas de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HeroePageComponent } from './heroes/pages/heroe-page/heroe-page.component';
-import { LayoutPageComponent } from './heroes/pages/layout-page/layout-page.component';
-import { ListPageComponent } from './heroes/pages/list-page/list-page.component';
-import { NewPageComponent } from './heroes/pages/new-page/new-page.component';
-import { SearchPageComponent } from './heroes/pages/search-page/search-page.component';
+// import { HeroePageComponent } from './heroes/pages/heroe-page/heroe-page.component';
+// import { LayoutPageComponent } from './heroes/pages/layout-page/layout-page.component';
+// import { ListPageComponent } from './heroes/pages/list-page/list-page.component';
+// import { NewPageComponent } from './heroes/pages/new-page/new-page.component';
+// import { SearchPageComponent } from './heroes/pages/search-page/search-page.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroePageComponent,
-    LayoutPageComponent,
-    ListPageComponent,
-    NewPageComponent,
-    SearchPageComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
     AppRoutingModule,
     SharedModule, ///Este es el unico de los modulos que pondremos aqui debido a que vamos a compartir elementos entre el resto de los componentes
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/pages/heroe-page/heroe-page.component.ts
+++ src/app/heroes/pages/heroe-page/heroe-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-heroe-page',
   templateUrl: './heroe-page.component.html',
   styles: ``
 })
-export class HeroePageComponent {
+
+export class HeroePageComponent implements OnInit {
+
+  public  hero?:  Hero;
+  constructor(
+    private heroesService:  HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        delay(1500),//tiempo de carga de la pagina
+        switchMap(  ({  id  })  =>  this.heroesService.getHeroById( id  )),
+      )
+      .subscribe( hero  =>  {
+        if  ( !hero ) return  this.router.navigate([  '/heroes/list'  ]);
+
+        this.hero = hero;
+        return;
+      })
+  }
+
+  goBack():void {
+    this.router.navigateByUrl('heroes/list')
+  }
 
 }
